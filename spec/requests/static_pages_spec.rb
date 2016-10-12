3819491520a1a5cc4d47760077d5fb0d74ce9346
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-10-12 16:43:51 +0100

    edit view help and home and add spec test for them

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rails_helper'
 
 describe "Static pages" do
 
@@ -7,6 +8,14 @@ describe "Static pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
     end
   end
 end
